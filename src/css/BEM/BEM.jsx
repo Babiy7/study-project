29ee761556dbd82e9fa1af3a8a6c425c05e933f7
Oleg Babiy <olegbabiy77@gmail.com>
2021-10-28 12:12:18 -0700

    Created BEM.

---
+++ src/css/BEM/BEM.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {} from 'prop-types';
+import './BEM.scss';
+
+function BEM(props) {
+
+    const miksAbout = (
+        <div className="about">
+            <div className="about__title title"></div>
+            <div className="about__subtitle subtitle"></div>
+        </div>
+    )
+
+    const about = (
+        <div className="about">
+            <div className="about__title"></div>
+            <div className="about__subtitle"></div>
+        </div>
+    )
+
+    const menu = (
+        <nav className="menu">
+            <a href="" className="menu__item menu__item_active"></a>
+            <a href="" className="menu__item"></a>
+            <a href="" className="menu__item"></a>
+            <a href="" className="menu__item"></a>
+            <a href="" className="menu__item"></a>
+        </nav>
+    )
+
+    return (
+        <div className="about">
+            <div className="about__body">
+                <div className="about__header">
+                    <div className="about__title title">
+                        About
+                    </div>
+                    <div className="about__text">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, provident aut dicta a, facere nobis ea vitae qui ex sint ipsum. Aliquam laudantium perferendis deleniti neque deserunt placeat, ipsa ad!
+                    </div>
+                </div>
+                <div className="about__row">
+                    <div className="about__column">
+                        <div className="item-about">
+                            <div className="item-about__icon"></div>
+                            <div className="item-about__title">Web</div>
+                            <div className="item-about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui iusto quam ducimus? Aspernatur vitae dolore deleniti molestias atque, quia incidunt, nemo consequatur velit placeat accusamus non tempora illum, quos nam.</div>
+                        </div>
+                    </div>
+                    <div className="about__column">
+                        <div className="item-about">
+                            <div className="item-about__icon"></div>
+                            <div className="item-about__title">Ios and android</div>
+                            <div className="item-about__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe omnis delectus mollitia molestiae incidunt error possimus debitis rerum, ipsa veritatis voluptates, ratione suscipit sapiente nam voluptate provident enim distinctio ipsum!</div>
+                        </div>
+                    </div>
+                    <div className="about__column">
+                        <div className="item-about">
+                            <div className="item-about__icon"></div>
+                            <div className="item-about__title">marketing</div>
+                            <div className="item-about__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima fugit iste sequi nam beatae corporis enim quibusdam illo, consequatur temporibus facilis commodi quam accusamus voluptatum at atque nihil aperiam nulla.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+BEM.propTypes = {
+
+}
+
+export default BEM;
+
