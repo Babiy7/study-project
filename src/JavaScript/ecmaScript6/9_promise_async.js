e8d8cfcda16e1f823d31adc004f0231349825645
oleh babiy <oleh.babiy@perfectial.com>
2021-11-09 13:07:07 +0200

    Changed a promise.

--- src/JavaScript/ecmaScript6/9_promise_async.js
+++ src/JavaScript/ecmaScript6/9_promise_async.js
@@ -1,23 +1,11 @@
-console.log("loading...");
-
-// const data = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("Data");
-//   }, 2000);
-// });
-
-// data.then((data) => {
-//   console.log(data);
-// });
-
 function delay(seconds) {
   return new Promise((resolve, reject) => {
     const number = Math.round(Math.random());
     setTimeout(() => {
       if (number) {
-        resolve("Success");
+        resolve('Success');
       } else {
-        reject("Error");
+        reject('Error');
       }
     }, seconds * 1000);
   });
@@ -30,3 +18,64 @@ delay(3)
   .catch((error) => {
     console.log(error);
   });
+
+function request(delay, isRejected) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isRejected) {
+        reject(`${delay} was rejected`);
+      } else {
+        resolve(`${delay} was resolved`);
+      }
+    }, delay * 1000);
+  });
+}
+
+const data = [
+  {
+    value: 1,
+    isRejected: false,
+  },
+  {
+    value: 3,
+    isRejected: false,
+  },
+  {
+    value: 2,
+    isRejected: false,
+  },
+];
+
+const promises = data.map(({ value, isRejected }) => {
+  return request(value, isRejected);
+});
+
+console.log('promises', promises);
+
+// Promise.all(promises)
+//   .then(console.log)
+//   .catch(console.log);
+
+// Promise.allSettled(promises)
+//   .then(console.log)
+//   .catch(console.log);
+
+Promise.race(promises)
+  .then(console.log)
+  .catch(console.log);
+
+async function fetchCustom() {
+  let result;
+  try {
+    await Promise.reject('erro').catch(console.log);
+    // result = await Promise.resolve('success');
+  } catch(e) {
+    console.log('e', e);
+  }
+
+  return result;
+}
+
+fetchCustom();
+  // .catch(e => console.log(e));
+
