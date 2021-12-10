90b9b530020ec3805b2e307ef0adaf8f72872a13
Oleg Babiy <olegbabiy77@gmail.com>
2021-12-10 06:27:17 -0800

    Added form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import BEM from './css/BEM/BEM';
+// import BEM from './css/BEM/BEM';
+import Form from './ReactJs/Form/Form';
+import Select from './ReactJs/Select/Select';
 import './App.scss';
 
 function App() {
@@ -12,8 +14,48 @@ function App() {
         <div className="item red" />
         <div className="item yellow" />
         <div className="item green" />
-        <div className="item blue" />
+        <div classsName="item blue" />
       </div> */}
+      <Form
+        inputsData={[
+          {
+            type: 'text',
+            id: 'name',
+            name: 'name',
+            placeholder: 'Type name',
+            required: true,
+            // можна добавляти регулярний вираз для кращої валідації
+            // pattern: \/\,
+          },
+          {
+            type: 'email',
+            id: 'email',
+            name: 'email',
+            placeholder: 'Type email',
+            required: true,
+          },
+          {
+            type: 'submit',
+            text: 'Save',
+          },
+        ]}
+      />
+      <Select
+        options={[
+          {
+            value: 'one',
+            option: 'One',
+          },
+          {
+            value: 'two',
+            option: 'Two',
+          },
+          {
+            value: 'three',
+            option: 'Three',
+          },
+        ]}
+      />
     </div>
   );
 }
---
+++ src/ReactJs/Form/Form.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import './Form.scss';
+
+function Form({ inputsData }) {
+  const form = useRef(null);
+
+  function setForm(form) {
+    const data = {};
+
+    function getData({ children, tagName }) {
+      if (tagName === 'FORM') {
+        getData(children[0]);
+      }
+
+      if (tagName === 'DIV') {
+        for (let i = 0; i < children.length; i++) {
+          getData(children[i]);
+        }
+      }
+
+      if (tagName === 'LABEL') {
+        data[children[0].name] = children[0].value;
+      }
+    }
+    getData(form);
+
+    return data;
+  }
+
+  function buildInput(element) {
+    if (['text', 'email'].includes(element.type)) {
+      return (
+        <label htmlFor={element.id}>
+          <input className="form__input" {...element} />
+        </label>
+      );
+    }
+
+    if (['submit'].includes(element.type)) {
+      return (
+        <button className="form__button" type={element.type}>
+          {element.text}
+        </button>
+      );
+    }
+  }
+
+  return (
+    <form
+      ref={form}
+      onSubmit={(e) => {
+        e.preventDefault();
+        const data = setForm(form.current);
+        console.log('data', data);
+      }}
+    >
+      <div className="form">
+        <div className="form__header">
+          <h3>Simple form</h3>
+        </div>
+        {inputsData.map((data) => buildInput(data))}
+      </div>
+    </form>
+  );
+}
+
+Form.defaultProps = {
+  inputsData: [],
+};
+
+Form.propTypes = {
+  inputsData: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+export default Form;
