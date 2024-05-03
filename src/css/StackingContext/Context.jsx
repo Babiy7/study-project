c8f9fb4b1a4ab57915aeba748b7a19e6add682ab
oleh <oleh.babiy@perfectial.com>
2024-05-03 15:52:25 +0300

    Added a config.

---
+++ src/css/StackingContext/Context.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styles from './Context.module.scss';
+
+export function Context(props) {
+  return (
+    <div className={styles.main}>
+      <div className={styles.div1}><h3 className={styles.textDiv1}>div 1</h3></div>
+      <div className={styles.div2}><h3 className={styles.textDiv2}>div 2</h3></div>
+      <div className={styles.div3}>
+        <h3 className={styles.textDiv3}>div 3</h3>
+        <div className={styles.div4}><h3 className={styles.textDiv4}>div 4</h3></div>
+        <div className={styles.div5}><h3 className={styles.textDiv5}>div 5</h3></div>
+        <div className={styles.div6}><h3 className={styles.textDiv6}>div 6</h3></div>
+      </div>
+      <div className={styles.description}>
+        <ul>
+          <li>div 1</li>
+          <li>div 2</li>
+          <li>
+            div 3
+            <ul>
+              <li>div 4</li>
+              <li>div 5</li>
+              <li>div 6</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
