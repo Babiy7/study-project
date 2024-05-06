79f46583a2ab3360ccf6b6ae8865c5e14015094c
oleh <oleh.babiy@perfectial.com>
2024-05-06 23:43:57 +0300

    Added a selection sort.

--- src/final-exam/Sorting/SelectionSort.js
+++ src/final-exam/Sorting/SelectionSort.js
@@ -1,26 +1,27 @@
-function selectionSort(arr) {
-  const len = arr.length;
-  for (let i = 0; i < len - 1; i++) {
+function selectionSort(array) {
+  const { length } = array;
+
+  for (let i = 0; i < length - 1; i++) {
     let minIndex = i;
-    for (let j = i + 1; j < len; j++) {
-      if (arr[j] < arr[minIndex]) {
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[minIndex]) {
         minIndex = j;
       }
     }
+
     if (minIndex !== i) {
-      // Обмін значень, якщо знайдено менший елемент
-      const temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
+      const currentValue = array[i];
+
+      array[i] = array[minIndex];
+      array[minIndex] = currentValue;
     }
   }
-  return arr;
+
+  return array;
 }
 
-// Приклад використання
-// const arr = [64, 25, 12, 22, 11];
-// console.log('Масив до сортування:', arr);
-// const sortedArr = selectionSort(arr);
-// console.log('Масив після сортування:', sortedArr);
+const array = [64, 25, 12, 22, 11];
+const sortedArray = selectionSort(array);
 
-console.log('', Object.is({ name: '' }));
+console.log('Масив після сортування:', sortedArray);
