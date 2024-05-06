b7b4556e21585ad79130639f849ea82a0de15483
oleh <oleh.babiy@perfectial.com>
2024-05-06 23:44:34 +0300

    Refactored code.

--- src/final-exam/Sorting/SelectionSort.js
+++ src/final-exam/Sorting/SelectionSort.js
@@ -24,4 +24,4 @@ function selectionSort(array) {
 const array = [64, 25, 12, 22, 11];
 const sortedArray = selectionSort(array);
 
-console.log('Масив після сортування:', sortedArray);
+console.log('sorted array:', sortedArray);
